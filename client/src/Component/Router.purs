d57e97aecfe56d498473f6da1e3e970741c800a0
Chehui Chou <deadshot465@gmail.com>
2021-08-18 15:09:01 +0900

    Complete Ch30.6

---
+++ client/src/Component/Router.purs
@@ -0,0 +1,56 @@
+module Component.Router where
+  
+import Prelude
+
+import CSS (color, white)
+import Data.Const (Const)
+import Data.Maybe (Maybe(..))
+import Data.Route (Route(..))
+import Effect.Aff.Class (class MonadAff)
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.CSS as HC
+import Type.Proxy (Proxy(..))
+
+type Input = Unit
+type Output = Void
+type State =
+  { route :: Route
+  }
+type Action = Void
+
+data Query a = Navigate Route a
+
+type PageSlot = H.Slot (Const Void) Void Unit
+
+type Slots =
+  ( logon :: PageSlot
+  , logoff :: PageSlot
+  , users :: PageSlot
+  , changePassword :: PageSlot
+  )
+
+_logon = Proxy :: Proxy "logon"
+_logoff = Proxy :: Proxy "logoff"
+_users = Proxy :: Proxy "users"
+_changePassword = Proxy :: Proxy "changePassword"
+
+component :: ∀ m. MonadAff m => H.Component Query Input Output m
+component = H.mkComponent
+  { initialState: \_ -> { route: Logon }
+  , render
+  , eval: H.mkEval H.defaultEval
+    { handleQuery = handleQuery
+    }
+  }
+
+handleQuery :: ∀ a m. MonadAff m => Query a -> H.HalogenM State Action Slots Output m (Maybe a)
+handleQuery = case _ of
+  Navigate route a -> H.modify_ _ { route = route } *> pure (Just a)
+
+render :: ∀ m. MonadAff m => State -> H.ComponentHTML Action Slots m
+render { route } = case route of
+  Logon -> HH.span [ HC.style $ color white ] [ HH.text "Logon" ]
+  Logoff -> HH.span [ HC.style $ color white ] [ HH.text "Logoff" ]
+  Users _ -> HH.span [ HC.style $ color white ] [ HH.text "Users" ]
+  ChangePassword -> HH.span [ HC.style $ color white ] [ HH.text "ChangePassword" ]
